0f1ab8444e80ec10336a85beed45ef780f12c38f
sowmyath012 <sowmyath012>
2023-08-31 18:01:27 +0530

    firstcommit

---
+++ src/test/java/base/TeamTest.java
@@ -0,0 +1,13 @@
+package base;
+
+import org.testng.annotations.Test;
+
+public class TeamTest {
+	
+		@Test
+		public void Hello() {
+			System.out.println("welcome");
+			System.out.println("Alex");
+			
+}
+	}
